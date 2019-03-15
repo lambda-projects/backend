599a09f3ad9e8d99432ae9afc2cc23f6f43cfae0
Shrey <ShreyasJothish>
2019-03-14 20:21:21 -0700

    Updated the template of Mapbox

--- ds/app_heatmap.py
+++ ds/app_heatmap.py
@@ -12,10 +12,14 @@ df = pd.read_csv('gun-violence-data_01-2013_03-2018_cleaned.csv')
 # Feature engineering to combine # of people killed or injured
 df['n_killed_injured'] = df['n_killed'] + df['n_injured']
 df = df[df['n_killed_injured'] > 3]
+df['locations_info'] = df.apply(lambda row: "Location: "+row.city_or_county+\
+        "<br>"+"Casualty: "+str(row.n_killed_injured)+\
+        "<br>"+"Date: " + str(row.date), axis=1)
 
 site_lat = df.latitude
 site_lon = df.longitude
-locations_name = df.city_or_county
+#locations_info = df.city_or_county
+locations_info = df.locations_info
 
 data = [
     go.Scattermapbox(
@@ -27,14 +31,16 @@ data = [
             color='rgb(165,18,18)',
             opacity=0.5
         ),
-        text=locations_name,
+        text=locations_info,
         hoverinfo='text'
     ),
     ]
         
 layout = go.Layout(
     title='Mass shooting across USA (Jan 2013-March 2018)',
-    autosize=True,
+    #autosize=True,
+    width=1000,
+    height=1000,
     hovermode='closest',
     showlegend=False,
     mapbox=go.layout.Mapbox(
@@ -45,8 +51,9 @@ layout = go.Layout(
             lon=-94
         ),
         pitch=0,
-        zoom=3,
-        style='light'
+        zoom=2,
+        #style='light'
+        style='mapbox://styles/shreyasjothish/cjt9fxodx07wf1fm9aj5qljip',
     ),
 )
 
