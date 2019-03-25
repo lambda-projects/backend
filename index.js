dde8251ee430a1a63702856888f04a4a823fb19f
Andrew Benedict <atbenedict@gmail.com>
2019-03-24 20:26:04 -0400

    newest try

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ const server = require("./server.js");
 
 const instance = process.env.PORT || 5000;
 
-server.listen(instance, () => {
-  console.log(`server running on port ${instance}`);
-});
+server.listen(instance, () => {});
